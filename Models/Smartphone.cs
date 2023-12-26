5464280c89e2daeced3ea9dc605e31d5b5f2ee45
Augusto Xavier <augustoxalbuquerque@gmail.com>
2023-12-26 20:47:33 -0300

    concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo {get; set;}
+        protected string IMEI {get; set;}
+        protected int Memoria {get; set;}
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -23,4 +29,3 @@ namespace DesafioPOO.Models
 
         public abstract void InstalarAplicativo(string nomeApp);
     }
-}
